82b71b8d67e45dd1cf712296956b7ce87bdb3ed3
Yusuf Hammed <ahmedyusuf.kboy.olatunji978@gmail.com>
2021-09-26 08:02:50 +0100

    Add project files.

---
+++ DateTime and Strings.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_With_Dates_and_Text
+{
+    public class DateTime_and_Strings
+    {
+        public static string ConsecutiveNumbers(string seriesNum)
+        {
+            
+            var numberArray = Array.ConvertAll (seriesNum.Split ('-'), int.Parse);
+
+            var increment = ( numberArray[0] < numberArray[1] ) ? 1 : -1;
+
+            for ( int i = 0; i < numberArray.Length - 1; i++ )
+            {
+                
+                if ( numberArray[i] + increment != numberArray[i + 1] )
+
+                    return $"Your input {seriesNum} are not consecutive numbers.";
+
+            }
+            return $"Your input {seriesNum} are consecutive numbers.";
+
+            //var uniqueSeries = new List<int> ();
+            //var nums = Console.ReadLine ();
+            //var seriesNum = nums.Split ('-');
+            //Array.Sort (numberArray);
+
+            //if ( string.IsNullOrEmpty (seriesNum) || string.IsNullOrWhiteSpace (seriesNum) )
+            //{
+            //    Console.WriteLine ("Invalid input"); 
+            //    continue;
+            //}
+
+            //numberArray = Array.ConvertAll (seriesNum.Split ('-'), int.Parse);
+
+            //increment = ( numberArray[0] < numberArray[1] ) ? 1 : -1;
+
+            //if  (seriesNum.SequenceEqual(numberArray))
+            //if (seriesNum = Array.Sort (numberArray);)
+        }
+
+        public static string DuplicateNumbers(string numSeries)
+        {
+            do
+            {
+                if ( string.IsNullOrEmpty (numSeries) || string.IsNullOrWhiteSpace (numSeries) )
+                {
+                    break;
+                }
+
+                else
+                {
+                    var arrayOfInput = Array.ConvertAll (numSeries.Split ('-'), int.Parse);
+
+                    for ( int i = 0; i < arrayOfInput.Length - 1; i++ )
+                    {
+                        if ( Array.IndexOf (arrayOfInput, arrayOfInput[i], i + 1) != -1 )
+
+                            return $"There is a duplicate of {arrayOfInput[i]} in the input. ";
+                    }
+
+                    return "There is no duplicate in the input";
+                }
+
+                //break;
+
+            } while ( true );
+
+            return " ";
+
+            //return "Duplicate Numbers";
+            
+        }
+
+        public static string TimeDeterminant()
+        {
+            return "Duplicate Numbers";
+            
+        }
+
+        public static string PascalCase()
+        {
+            return "Duplicate Numbers";
+            
+        }
+
+        public static string EnglishWord()
+        {
+            return "Duplicate Numbers";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Working_With_Dates_and_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine (@"Make a  selection of the program you want to access from the follow: 
+- Input 'C' for Consecutive Numbers;
+- Input 'D' for  Duplicate of Numbers;
+- Input 'T' for Time Determinant;
+- Jnput 'P' for Pascal Case;
+- Input 'E' for English Word");
+
+                var selection = (string)Console.ReadLine (); // Making selection of the program to access.
+
+
+                const string consecutive = "c";
+                const string duplicate = "d";
+                const string time = "t";
+                const string pascal = "p";
+                const string english = "e";
+
+                switch ( selection.ToLower () )
+                {
+                    case consecutive:
+                        Console.WriteLine ();
+                        Console.WriteLine (@"You selected Consecutive Numbers.
+This program displays whether the user input are consecutive numbers or not.");
+                        Console.WriteLine ("Enter a few numbers separated by hyphen");
+                        //Console.WriteLine();
+                        var nums = Console.ReadLine ();
+                        Console.WriteLine (DateTime_and_Strings.ConsecutiveNumbers (nums)); // Accessing ConsecutiveNumbers method of DateTime_and_Strings class
+                        Console.WriteLine ();
+                        continue;
+
+                    case duplicate:
+                        Console.WriteLine ();
+                        Console.WriteLine (@"You selected Duplicate of Numbers
+This program display the duplicate of the user input.");
+                        Console.WriteLine ("Enter a few numbers separated by hyphen. ");
+                        var numberInput = Console.ReadLine ();
+                        Console.WriteLine (DateTime_and_Strings.DuplicateNumbers (numberInput)); // Accessing DuplicateNumbers method of DateTime_and_Strings class
+                        Console.WriteLine ();
+                        continue;
+
+                    case time:
+                        Console.WriteLine ();
+                        Console.WriteLine (@"You selected Time Determinant
+This program determine the validity of  time value input.");
+                        Console.WriteLine ("Enter a time value in 24 hours format (e.g 19:00). ");
+                        //Console.WriteLine();
+                        Console.WriteLine (DateTime_and_Strings.TimeDeterminant ()); // Accessing TimeDeterminant method of DateTime_and_Strings class
+                        Console.WriteLine ();
+                        continue;
+
+                    case pascal:
+                        Console.WriteLine ();
+                        Console.WriteLine ("You selected Pascal Case");
+                        Console.WriteLine ("Enter a few words separated by space. ");
+                        //Console.WriteLine();
+                        Console.WriteLine (DateTime_and_Strings.PascalCase ()); // Accessing PascalCase method of DateTime_and_Strings class
+                        Console.WriteLine ();
+                        continue;
+
+                    case english:
+                        Console.WriteLine ();
+                        Console.WriteLine ("You selected English Word");
+                        Console.WriteLine ("Enter an English word. ");
+                        //Console.WriteLine();
+                        Console.WriteLine (DateTime_and_Strings.EnglishWord ()); // Accessing EnglishWord method of DateTime_and_Strings class
+                        Console.WriteLine ();
+                        continue;
+
+                    default:
+                        Console.WriteLine ();
+                        Console.WriteLine ("You've made an invalid selection");
+                        Console.WriteLine ();
+                        break;
+                }
+                break;
+
+
+            } while ( true );
+
+
+        }
+
+
+    }
+
+
+}
